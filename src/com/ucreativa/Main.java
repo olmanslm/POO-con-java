4adf6fdb7a2885125cf77a1a635a2ab2f39fee14
Olman San Lee <olmans@thehangar.cr>
2021-01-25 18:36:56 -0600

    Initial commit

---
+++ src/com/ucreativa/Main.java
@@ -0,0 +1,9 @@
+package com.ucreativa;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to the class");
+    }
+}
